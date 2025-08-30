a18ca21ac49ddbc107f38966fde8709d52aaedc6
Monu Kushawaha <91782501+MONUkushawaha987@users.noreply.github.com>
2025-08-30 15:59:28 +0530

    app.js file is updated

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const http = require("http");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
+const reactAppUrl = process.env.REACT_APP_URL;
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: reactAppUrl,
     methods: ["GET", "POST"],
     credentials: true,
   },
